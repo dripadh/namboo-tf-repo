158a37aea20a00db8a87a440a8705c0db779bc87
Namboo <namboodripadh@gmail.com>
2025-04-07 13:05:40 +0530

    Added VM resource to main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,22 @@
 provider "google" {
-  project     = "extreme-tooling-455910-t0"  # ← change this!
+  project     = "extreme-tooling-455910-t0"  # 👈 Replace this
   region      = "us-central1"
   zone        = "us-central1-a"
+}
+
+resource "google_compute_instance" "namboo-vm" {
+  name         = "namboo-vm"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
 }
